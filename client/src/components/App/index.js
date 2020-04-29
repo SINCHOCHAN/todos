c4b56d9c9a2d7f561a3cb68d599fe8675c584a49
Terbee <terencechan@berkeley.edu>
2020-04-29 13:22:17 -0700

    first commit

---
+++ client/src/components/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Counter from '../../containers/Counter';
+import TodoForm from '../../containers/TodoForm';
+
+const App = () => (
+  <>
+    <Counter/>
+    <TodoForm/>
+  </>
+);
+
+export default App;
---
+++ client/src/components/RenderTodoList/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const RenderTodoList = props => {
+
+  const renderTodoListItems = () => {
+    if (props.items.length === 0) {
+      return <h1>No todos yet</h1>;
+    } else {
+      return props.items.map(todo => {
+        return (
+          <div key={todo.id}>
+            <li style={{ color: todo.completed ? 'blue' : 'red' }} >{todo.text}</li>
+            <button onClick={ () => props.handleDelete(todo.id) }>Delete</button>
+            <button onClick={ () => props.handleUpdateCompletedTodo(todo.id) }>Update</button>
+          </div>
+        )
+      });
+    }
+  }
+  return (
+    <ul>
+      { renderTodoListItems() }
+    </ul>
+  );
+};
+export default RenderTodoList;
